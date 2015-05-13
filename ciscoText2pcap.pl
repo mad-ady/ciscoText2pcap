e1828dc3abf5abf141646ab9a8b47098af779f3c
Adrian Popa <adrian.popa.gh@gmail.com>
2015-05-13 13:25:24 +0300

    Added ciscoText2pcap.pl script

---
+++ ciscoText2pcap.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+#Convert a text dump like this:
+#12:35:21.717 EET Apr 24 2015 : IPv4 LES CEF    : Ce0 None
+#
+#87526540:                            45C0004C              E@.L
+#87526550: 00000000 FD11C63E 0A303001 AC1F1052  ....}.F>.00.,..R
+#87526560: 007B007B 00385EBF 24040AEE 00000DEA  .{.{.8^?$..n...j
+#87526570: 000028A4 C1E767CD D8E48CD6 519D50B0  ..($AggMXd.VQ.P0
+#87526580: D8E48CD9 A9855D94 D8E48CD9 C161FAF9  Xd.Y).].Xd.YAazy
+#87526590: 00                                   .
+
+#... To pcap-friendly format
+#http://www.networkers-online.com/blog/2015/02/text2pcap-how-to-converts-ascii-dumps-to-pcap-files/
+
+print STDERR "Input text via STDIN\n";
+
+my $packetStarted = 0;
+while(<STDIN>){
+    my $line = $_;
+    if($line=~/[0-9]+:\s+((?:[0-9A-F]+ ){1,4}) /){
+	my $section = $1;
+	my @sections = split(/ /, $section);
+	
+	#print "DBG: $line\n";
+	#print Dumper(\@sections);
+	
+	foreach my $block (@sections){
+	    if($block=~/(..)(..)?(..)?(..)?/){
+		my $thing = "$1 $2 $3 $4 ";
+		my $offset = sprintf("%08x", $packetStarted);
+		print "$offset $thing\n";
+		$packetStarted +=1;
+		if(defined $2){
+		    $packetStarted +=1;
+		}
+		if(defined $3){
+		    $packetStarted +=1;
+		}
+		if(defined $4){
+		    $packetStarted +=1;
+		}
+	    }
+	}
+	
+    }
+    else{
+	#signal an end of packet
+	
+	$packetStarted = 0;
+    }
+}
